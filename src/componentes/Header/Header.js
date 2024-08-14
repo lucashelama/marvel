fe59d2453cfabce3cf26e5c807fc0a8ccf805c7e
Lucas Helamã <lhelama@gmail.com>
2024-08-14 12:49:31 -0300

    Desafio Front End

---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo/Group.png"
+import lupa from "../../assets/busca/Lupa/Shape.png"
+
+function Header() {
+    return (
+        <header>
+            <img src={logo} alt=""></img>
+            <h1>Explore o Universo</h1>
+            <p>Mergulhe no dominio deslumbrante de todos os personagens classicos que você ama</p>
+            <div>
+                <img src={lupa} alt="icone"></img>
+                <input type="text" placeholder="procure por heróis" ></input>
+            </div>
+
+        </header>)
+}
+
+export default Header;
